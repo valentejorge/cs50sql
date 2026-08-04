67f8243d728618cc07827b0bfb6623b73df640fa
Jorge Valente Jr <62824468+valentejorge@users.noreply.github.com>
2026-08-04 23:58:05 +0000

    Tue, Aug 4, 2026, 8:58 PM -03:00

--- pset0/cyberchase/8.sql
+++ pset0/cyberchase/8.sql
@@ -1,1 +1,1 @@
-SELECT COUNT()"title" FROM "episodes" WHERE "air_date" BETWEEN '2018-01-01' AND '2023-12-31';
+SELECT COUNT("title") AS "Total Titles" FROM "episodes" WHERE "air_date" BETWEEN '2018-01-01' AND '2023-12-31';
