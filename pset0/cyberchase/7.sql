d52512d7b6d2a244cbd8bd2a0d95abb50c21187b
Jorge Valente Jr <62824468+valentejorge@users.noreply.github.com>
2026-08-04 21:11:11 -0300

    pset0: Add SQL queries cyberchase

--- pset0/cyberchase/7.sql
+++ pset0/cyberchase/7.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 SELECT "title", "topic" FROM "episodes" WHERE "topic" LIKE '%fractions%';
-=======
-SELECT "title", "topic" FROM "episodes" WHERE "topic" LIKE '%fractions%';
->>>>>>> 8dc592a389888af467e292b8d0fec1b79625823d
--- pset0/cyberchase/8.sql
+++ pset0/cyberchase/8.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 SELECT COUNT("title") AS "Total Titles" FROM "episodes" WHERE "air_date" BETWEEN '2018-01-01' AND '2023-12-31';
-=======
-SELECT COUNT("title") AS "Total Titles" FROM "episodes" WHERE "air_date" BETWEEN '2018-01-01' AND '2023-12-31';
->>>>>>> 8dc592a389888af467e292b8d0fec1b79625823d
